a04cbdad6b73479604d226ff0333ed2e7386e157
Jin-Woo Kim <dnehd6739@naver.com>
2022-07-20 14:32:09 +0900

    update

--- C++PS/Project1/기본.cpp
+++ C++PS/Project1/기본.cpp
@@ -11,6 +11,37 @@
 #include <sstream>
 
 using namespace std;
+
+/* 백준 : 11403 
+int N;
+int graph[101][101];
+void floyd() {
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (graph[i][k] && graph[k][j])
+					graph[i][j] = 1;
+			}
+		}
+	}
+}
+
+int main() {
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> graph[i][j];
+		}
+	}
+	floyd();
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cout << graph[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+*/
 /* 백준 : 10973 
 int N;
 int main() {
